c23f0ab3866b77e400dccaa6b296baa9fa0c756f
Rogoura <rodrigorogouraoficial@gmail.com>
2024-07-18 19:51:43 -0300

    Misericordia

---
+++ Aula05_Estruturas_Condicionais/DesafioCaiqueAcademiaJuntoFIT.py
@@ -0,0 +1,9 @@
+presenca = int(input("Frequência do aluno: "))  
+
+dias = 21
+print("VOCÊ ESTÁ PARTICIPANDO DA NOSSA PROMO TREINA JUNTO")
+
+if presenca >= 21 & presenca >= 200:  
+    print("UHUU. AGORA VOCÊ PODE PRESENTEAR UM AMIGO OU AMIGA PARA TREINAR COM VOCÊ")
+elif presenca > 0 and presenca < 21: 
+    print("QUE BOM VER VOCÊ DE VOLTA. A PARTIR DE AGORA INICIAMOS MAIS UMA CONTAGEM DE 21 DIAS PARA A PROMO TREINA JUNTO.")
